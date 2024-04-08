1c48583b9e08bef151f8c59aa5b8e39dd4e3d696
TanvirAlHasib <tanviralhasib1@gmail.com>
2024-04-08 23:26:56 +0600

    init: project environment initialized

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+
+  return <>
+
+  <div className="flex w-full h-full justify-center items-center">
+    <h1 className="text-black font-semibold text-7xl">Project initialized</h1>
+  </div>
+
+  </>
+}
+
+export default App
