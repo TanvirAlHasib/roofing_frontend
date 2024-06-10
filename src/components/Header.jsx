89351c21f826f7ecdf74cc12bbf43778b6cf8610
TanvirAlHasib <tanviralhasib1@gmail.com>
2024-06-10 16:21:39 +0600

    bug: fixed cart bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -62,9 +62,10 @@ export default function Header() {
                       <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
 
-                    <span className="absolute -top-2.5 -right-2.5 inline-flex items-center justify-center gap-1 rounded-full border-2 border-white bg-pink-500 px-1.5 text-sm text-white">
-                      {Object.keys(cart).length ? Object.keys(cart).length - 1: ""}
-                    </span>
+                    {Object.keys(cart).length ? <span className="absolute -top-2.5 -right-2.5 inline-flex items-center justify-center gap-1 rounded-full border-2 border-white bg-pink-500 px-1.5 text-sm text-white">
+                      {Object.keys(cart).length - 1}
+                      </span>: ""}
+
                   </NavLink>
 
                   {/* end here Rounded base sized cart logo --> */}
